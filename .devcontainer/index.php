d23eebb03a11daa458a495e21bd409199dd7d50d
Grayson Miller <104777457+vertigo-one@users.noreply.github.com>
2024-09-27 17:08:04 -0500

    fix php cert target
    Signed-off-by: Grayson Miller <104777457+vertigo-one@users.noreply.github.com>

--- .devcontainer/index.php
+++ .devcontainer/index.php
@@ -6,7 +6,7 @@
     <body>
         <?php echo '<p>The currently in use SSL certificate is displayed below</p>'; ?>
         <?php
-          $cmd = "openssl x509 -inform pem -noout -text -in /var/www/html/cert.pem";
+          $cmd = "openssl x509 -inform pem -noout -text -in /etc/ssl/certs/cert.crt";
           $result = shell_exec($cmd);
          echo '<pre>';
          echo $result;
